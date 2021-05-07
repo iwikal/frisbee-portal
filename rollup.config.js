b73a5c6b21d9d3b48ca1ca38e2d30ed1083498cf
Joel Nordström <e.joel.nordstrom@gmail.com>
2021-05-07 20:06:03 +0200

    Fix 404

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,13 @@
+import typescript from '@rollup/plugin-typescript'
+
 export default ({
   input: 'src/index.ts',
   output: {
     file: 'build/index.js',
     format: 'iife',
     sourcemap: true,
-  }
+  },
+  plugins: [
+    typescript()
+  ]
 })
