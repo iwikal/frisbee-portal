a9c2a2721f58dda70075d172a4d1feb9cad40e70
Joel Nordström <e.joel.nordstrom@gmail.com>
2021-05-08 20:01:18 +0200

    Prettify some GameEntity related code

--- client/world.ts
+++ client/world.ts
@@ -2,9 +2,24 @@ import { GameEntity } from "./gamestate";
 import { Transform, Vector } from "../shared/vector";
 
 export class Wall extends GameEntity {
-
   constructor(size: Vector, transform: Transform) {
-    super(size, transform, {style: (ctx)=>{const g = ctx.createLinearGradient(-size.x / 2, -size.y / 2, size.x, - size.y / 2); g.addColorStop(0, "black"); g.addColorStop(1, "red"); return g}})
+    super(
+      size,
+      transform,
+      {
+        style: (ctx) => {
+          const g = ctx.createLinearGradient(
+            -size.x / 2,
+            -size.y / 2,
+            size.x,
+            -size.y / 2,
+          )
+          g.addColorStop(0, "black")
+          g.addColorStop(1, "red")
+          return g
+        },
+      }
+    )
   }
 }
 
